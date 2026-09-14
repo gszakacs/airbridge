cb30af40bc21d0d692ca2f87050eb0f0b33f0a22
gszakacs <gszakacs@users.noreply.github.com>
2026-09-13 20:38:53 -0400

    Wire status LED into existing monitor lifecycle

--- src/status_led.h
+++ src/status_led.h
@@ -4,5 +4,4 @@
 void StatusLed_init();
 
 // Non-blocking status indication. Call from loop().
-// airsense_present is the most recent health-poll result.
-void StatusLed_tick(bool airsense_present);
+void StatusLed_tick();
